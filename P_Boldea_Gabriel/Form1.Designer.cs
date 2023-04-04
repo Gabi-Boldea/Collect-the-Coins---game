c7d0b756da6a7ed38021304d158ea2c1562797fc
Gabi-Boldea <gabriel.boldea02@gmail.com>
2023-04-04 23:23:21 +0300

    first commit

---
+++ P_Boldea_Gabriel/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace P_Boldea_Gabriel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.puncte = new System.Windows.Forms.Label();
+            this.door = new System.Windows.Forms.PictureBox();
+            this.coin5 = new System.Windows.Forms.PictureBox();
+            this.coin6 = new System.Windows.Forms.PictureBox();
+            this.coin4 = new System.Windows.Forms.PictureBox();
+            this.coin3 = new System.Windows.Forms.PictureBox();
+            this.coin2 = new System.Windows.Forms.PictureBox();
+            this.coin1 = new System.Windows.Forms.PictureBox();
+            this.platform5 = new System.Windows.Forms.PictureBox();
+            this.platform4 = new System.Windows.Forms.PictureBox();
+            this.platform3 = new System.Windows.Forms.PictureBox();
+            this.platform6 = new System.Windows.Forms.PictureBox();
+            this.platform2 = new System.Windows.Forms.PictureBox();
+            this.platform1 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.background = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.door)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = global::P_Boldea_Gabriel.Properties.Resources.umbra;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 24);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Monede ramase:";
+            // 
+            // puncte
+            // 
+            this.puncte.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.puncte.AutoSize = true;
+            this.puncte.BackColor = System.Drawing.Color.Transparent;
+            this.puncte.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puncte.Image = global::P_Boldea_Gabriel.Properties.Resources.umbra;
+            this.puncte.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.puncte.Location = new System.Drawing.Point(174, 9);
+            this.puncte.Name = "puncte";
+            this.puncte.Size = new System.Drawing.Size(21, 24);
+            this.puncte.TabIndex = 15;
+            this.puncte.Text = "6";
+            // 
+            // door
+            // 
+            this.door.BackColor = System.Drawing.Color.Transparent;
+            this.door.Image = global::P_Boldea_Gabriel.Properties.Resources.d1_removebg_preview;
+            this.door.Location = new System.Drawing.Point(892, 36);
+            this.door.Name = "door";
+            this.door.Size = new System.Drawing.Size(103, 94);
+            this.door.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.door.TabIndex = 14;
+            this.door.TabStop = false;
+            // 
+            // coin5
+            // 
+            this.coin5.BackColor = System.Drawing.Color.Transparent;
+            this.coin5.Image = global::P_Boldea_Gabriel.Properties.Resources.c2;
+            this.coin5.Location = new System.Drawing.Point(397, 269);
+            this.coin5.Name = "coin5";
+            this.coin5.Size = new System.Drawing.Size(37, 38);
+            this.coin5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin5.TabIndex = 13;
+            this.coin5.TabStop = false;
+            this.coin5.Tag = "coin";
+            // 
+            // coin6
+            // 
+            this.coin6.BackColor = System.Drawing.Color.Transparent;
+            this.coin6.Image = global::P_Boldea_Gabriel.Properties.Resources.c2;
+            this.coin6.Location = new System.Drawing.Point(726, 248);
+            this.coin6.Name = "coin6";
+            this.coin6.Size = new System.Drawing.Size(37, 38);
+            this.coin6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin6.TabIndex = 12;
+            this.coin6.TabStop = false;
+            this.coin6.Tag = "coin";
+            // 
+            // coin4
+            // 
+            this.coin4.BackColor = System.Drawing.Color.Transparent;
+            this.coin4.Image = global::P_Boldea_Gabriel.Properties.Resources.c2;
+            this.coin4.Location = new System.Drawing.Point(208, 92);
+            this.coin4.Name = "coin4";
+            this.coin4.Size = new System.Drawing.Size(37, 38);
+            this.coin4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin4.TabIndex = 11;
+            this.coin4.TabStop = false;
+            this.coin4.Tag = "coin";
+            // 
+            // coin3
+            // 
+            this.coin3.BackColor = System.Drawing.Color.Transparent;
+            this.coin3.Image = global::P_Boldea_Gabriel.Properties.Resources.c2;
+            this.coin3.Location = new System.Drawing.Point(165, 92);
+            this.coin3.Name = "coin3";
+            this.coin3.Size = new System.Drawing.Size(37, 38);
+            this.coin3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin3.TabIndex = 10;
+            this.coin3.TabStop = false;
+            this.coin3.Tag = "coin";
+            // 
+            // coin2
+            // 
+            this.coin2.BackColor = System.Drawing.Color.Transparent;
+            this.coin2.Image = global::P_Boldea_Gabriel.Properties.Resources.c2;
+            this.coin2.Location = new System.Drawing.Point(122, 92);
+            this.coin2.Name = "coin2";
+            this.coin2.Size = new System.Drawing.Size(37, 38);
+            this.coin2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin2.TabIndex = 9;
+            this.coin2.TabStop = false;
+            this.coin2.Tag = "coin";
+            // 
+            // coin1
+            // 
+            this.coin1.BackColor = System.Drawing.Color.Transparent;
+            this.coin1.Image = global::P_Boldea_Gabriel.Properties.Resources.c2;
+            this.coin1.Location = new System.Drawing.Point(79, 92);
+            this.coin1.Name = "coin1";
+            this.coin1.Size = new System.Drawing.Size(37, 38);
+            this.coin1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin1.TabIndex = 8;
+            this.coin1.TabStop = false;
+            this.coin1.Tag = "coin";
+            // 
+            // platform5
+            // 
+            this.platform5.BackColor = System.Drawing.Color.Transparent;
+            this.platform5.Image = global::P_Boldea_Gabriel.Properties.Resources.p3;
+            this.platform5.Location = new System.Drawing.Point(849, 117);
+            this.platform5.Name = "platform5";
+            this.platform5.Size = new System.Drawing.Size(185, 54);
+            this.platform5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.platform5.TabIndex = 7;
+            this.platform5.TabStop = false;
+            this.platform5.Tag = "platform";
+            // 
+            // platform4
+            // 
+            this.platform4.BackColor = System.Drawing.Color.Transparent;
+            this.platform4.Image = global::P_Boldea_Gabriel.Properties.Resources.p3;
+            this.platform4.Location = new System.Drawing.Point(675, 292);
+            this.platform4.Name = "platform4";
+            this.platform4.Size = new System.Drawing.Size(138, 54);
+            this.platform4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.platform4.TabIndex = 6;
+            this.platform4.TabStop = false;
+            this.platform4.Tag = "platform";
+            // 
+            // platform3
+            // 
+            this.platform3.BackColor = System.Drawing.Color.Transparent;
+            this.platform3.Image = global::P_Boldea_Gabriel.Properties.Resources.p3;
+            this.platform3.Location = new System.Drawing.Point(572, 473);
+            this.platform3.Name = "platform3";
+            this.platform3.Size = new System.Drawing.Size(156, 54);
+            this.platform3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.platform3.TabIndex = 5;
+            this.platform3.TabStop = false;
+            this.platform3.Tag = "platform";
+            // 
+            // platform6
+            // 
+            this.platform6.BackColor = System.Drawing.Color.Transparent;
+            this.platform6.Image = global::P_Boldea_Gabriel.Properties.Resources.p3;
+            this.platform6.Location = new System.Drawing.Point(60, 136);
+            this.platform6.Name = "platform6";
+            this.platform6.Size = new System.Drawing.Size(210, 54);
+            this.platform6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.platform6.TabIndex = 4;
+            this.platform6.TabStop = false;
+            this.platform6.Tag = "platform";
+            // 
+            // platform2
+            // 
+            this.platform2.BackColor = System.Drawing.Color.Transparent;
+            this.platform2.Image = global::P_Boldea_Gabriel.Properties.Resources.p3;
+            this.platform2.Location = new System.Drawing.Point(343, 313);
+            this.platform2.Name = "platform2";
+            this.platform2.Size = new System.Drawing.Size(148, 54);
+            this.platform2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.platform2.TabIndex = 3;
+            this.platform2.TabStop = false;
+            this.platform2.Tag = "platform";
+            // 
+            // platform1
+            // 
+            this.platform1.BackColor = System.Drawing.Color.Transparent;
+            this.platform1.Image = global::P_Boldea_Gabriel.Properties.Resources.p3;
+            this.platform1.Location = new System.Drawing.Point(102, 473);
+            this.platform1.Name = "platform1";
+            this.platform1.Size = new System.Drawing.Size(156, 54);
+            this.platform1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.platform1.TabIndex = 2;
+            this.platform1.TabStop = false;
+            this.platform1.Tag = "platform";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::P_Boldea_Gabriel.Properties.Resources.j;
+            this.player.Location = new System.Drawing.Point(12, 579);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(64, 70);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // background
+            // 
+            this.background.BackColor = System.Drawing.SystemColors.Control;
+            this.background.Image = global::P_Boldea_Gabriel.Properties.Resources.f2;
+            this.background.Location = new System.Drawing.Point(-5, -2);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(1700, 750);
+            this.background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.background.TabIndex = 0;
+            this.background.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 661);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.puncte);
+            this.Controls.Add(this.door);
+            this.Controls.Add(this.coin5);
+            this.Controls.Add(this.coin6);
+            this.Controls.Add(this.coin4);
+            this.Controls.Add(this.coin3);
+            this.Controls.Add(this.coin2);
+            this.Controls.Add(this.coin1);
+            this.Controls.Add(this.platform5);
+            this.Controls.Add(this.platform4);
+            this.Controls.Add(this.platform3);
+            this.Controls.Add(this.platform6);
+            this.Controls.Add(this.platform2);
+            this.Controls.Add(this.platform1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.background);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "Form1";
+            this.Text = "Platforma";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.door)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox platform1;
+        private System.Windows.Forms.PictureBox platform2;
+        private System.Windows.Forms.PictureBox platform6;
+        private System.Windows.Forms.PictureBox platform3;
+        private System.Windows.Forms.PictureBox platform4;
+        private System.Windows.Forms.PictureBox platform5;
+        private System.Windows.Forms.PictureBox coin1;
+        private System.Windows.Forms.PictureBox coin2;
+        private System.Windows.Forms.PictureBox coin3;
+        private System.Windows.Forms.PictureBox coin4;
+        private System.Windows.Forms.PictureBox coin6;
+        private System.Windows.Forms.PictureBox coin5;
+        private System.Windows.Forms.PictureBox door;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label puncte;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ P_Boldea_Gabriel/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P_Boldea_Gabriel
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer sunet = new SoundPlayer(Properties.Resources.smrpg_coin);
+        public Form1()
+        {
+            InitializeComponent();
+            door.Visible = false;
+            player.Parent = background;
+            platform1.Parent = background;
+            platform2.Parent = background;
+            platform3.Parent = background;
+            platform4.Parent = background;
+            platform5.Parent = background;
+            platform6.Parent = background;
+            coin1.Parent = background;
+            coin2.Parent = background;
+            coin3.Parent = background;
+            coin4.Parent = background;
+            coin5.Parent = background;
+            coin6.Parent = background;
+            door.Parent = background;
+            label1.Parent = background;
+            puncte.Parent = background;
+        }
+
+        #region .. Double Buffered function ..
+        public static void SetDoubleBuffered(System.Windows.Forms.Control c)
+        {
+            if (System.Windows.Forms.SystemInformation.TerminalServerSession)
+                return;
+            System.Reflection.PropertyInfo aProp = typeof(System.Windows.Forms.Control).GetProperty("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            aProp.SetValue(c, true, null);
+        }
+        #endregion
+
+        #region .. code for Flickering ..
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;
+                return cp;
+            }
+        }
+        #endregion
+
+        bool sus=false, dreapta=false, stanga=false;
+        int viteza = 15;
+        int cadere = 5;
+        int pozitie = 1;
+        int pu = 6; //puctaj
+        //int forta=8;
+        bool ok = false;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.KeyCode == Keys.Up)
+            {
+                sus = true;
+            }
+            
+            if (e.KeyCode == Keys.Left)
+                stanga = true;
+            if (e.KeyCode == Keys.Right)
+                dreapta = true;
+            /*
+            if (e.KeyCode == Keys.Up && !sus)
+                sus = true;
+            */
+            if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                sus = false;
+            if (e.KeyCode == Keys.Left)
+                stanga = false;
+            if (e.KeyCode == Keys.Right)
+                dreapta = false;
+            /*
+            if (sus)
+                sus = false;
+            */
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            while (player.Right + background.Left > 550 && background.Left > -600)
+            {
+                background.Left--;
+                label1.Left++;
+                puncte.Left++;
+            }
+
+            while (player.Left + background.Left < 60 && background.Left < 0)
+            {
+                background.Left++;
+                label1.Left--;
+                puncte.Left--;
+            }
+            
+            if (player.Top < this.Height - player.Height - 50)
+            {
+                player.Top += cadere;
+            }
+            /*
+            //player.Top += viteza;
+            if (sus && forta < 0)
+                sus = false;
+            */
+            if (sus)
+            {
+                player.Top -= viteza;
+                //viteza = -12;
+                //forta -= 1;
+            }
+            else
+                viteza = 12;
+            if (dreapta)
+            {
+                //player.Left += 5;
+                player.Left += viteza;
+                if (pozitie == 0)
+                {
+                    player.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+                    pozitie = 1;
+                }
+                if (player.Left > background.Right - player.Width - 10)
+                    player.Left = background.Right - player.Width -5;
+            }
+            if (stanga)
+            {
+                //player.Left -= 5;
+                player.Left -= viteza;
+                if (pozitie == 1)
+                {
+                    player.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+                    pozitie = 0;
+                }
+                if (player.Left < 5)
+                    player.Left = player.Left + 15;
+            }
+            foreach (Control x in background.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "platform")
+                {
+                    if (x.Bounds.IntersectsWith(player.Bounds) && !sus)
+                    {
+                        cadere = 8;
+                        //forta = 8;
+                        player.Top = x.Top - player.Height;
+                    }
+                }
+            }
+            //banuti
+            foreach (Control x in background.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "coin")
+                {
+                    if (x.Bounds.IntersectsWith(player.Bounds) && x.Visible == true)
+                    {
+                        sunet.Play();
+                        x.Visible = false;
+                        pu--;
+                        puncte.Text = Convert.ToString(pu);
+                        if (pu == 0)
+                        {
+                            door.Visible = true;
+                        }
+                    }
+                }
+                if (door.Bounds.IntersectsWith(player.Bounds) && door.Visible == true)
+                {
+                    if (ok == false)
+                    {
+                        //this.Visible = false;
+                        //F2 g = new F2(pu);
+                        //g.ShowDialog();
+                        //this.Close();
+                        ok = true;
+                        timer1.Stop();
+                        MessageBox.Show("Ai castigat!");
+                    }
+                }
+            }
+        }
+    }
+}
